5535b3a9ee3ca43e41dba243f1967b69b45bc056
atayarova <r.atayarova@gmail.com>
2022-10-25 00:13:34 +0600

    test

--- HW08 - DML/SQLQuery result save.sql
+++ HW08 - DML/SQLQuery result save.sql
@@ -1,1 +1,6 @@
 exec master..xp_cmdshell 'bcp "SELECT FullName, PreferredName FROM WideWorldImporters.Application.People ORDER BY FullName" queryout "D:\Rosedocs\DATA\People.txt" -t, -c -T'
+
+
+declare @sql varchar(8000) 
+select @sql  = 'bcp "SELECT FullName, PreferredName FROM WideWorldImporters.Application.People ORDER BY FullName" queryout "D:\Rosedocs\DATA\People.txt" -T -S' + @@servername + '-c -t'
+exec master..xp_cmdshell @sql
